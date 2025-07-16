0d7e6dfec565af91419a9b789f9600a84b99752e
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-16 12:26:21 +0530

    Explored get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the other side");
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the other side");
+// });
+
+//GET
+app.get("/test", (req, res) => {
+  res.send("HAHAHAHA this is a get request");
 });
 
-app.get("/profile", (req, res)=>{
-  res.send("Namaste from the profile side");
-})
+//POST
+app.post("/test", (req, res) => {
+  res.send({firstName : "Sushant", lastName : "Tiwari"});
+});
+
+//DELETE
+app.delete("/test", (req, res) => {
+  res.send("This is a delete request");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
