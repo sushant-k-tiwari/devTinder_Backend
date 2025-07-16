38b1f2f136790553ad4fce0beb618fa0bc3ed7a4
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-16 12:37:01 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,14 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/test", (req, res) => {
-//   res.send("Hello from the other side");
-// });
-
 //GET
-app.get("/test", (req, res) => {
+app.get("/test/:userId/:name/:age/", (req, res) => {
+    // console.log(req.query)
+    console.log(req.params)
   res.send("HAHAHAHA this is a get request");
 });
 
-//POST
-app.post("/test", (req, res) => {
-  res.send({firstName : "Sushant", lastName : "Tiwari"});
-});
 
-//DELETE
-app.delete("/test", (req, res) => {
-  res.send("This is a delete request");
-});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
