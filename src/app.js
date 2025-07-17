6dc7834dd09abb59805b616c18410a20f2eb206c
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-17 11:34:48 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,13 +3,33 @@ const express = require("express");
 const app = express();
 
 //GET
-app.get("/test/:userId/:name/:age/", (req, res) => {
-    // console.log(req.query)
-    console.log(req.params)
-  res.send("HAHAHAHA this is a get request");
-});
-
-
+app.get(
+  "/test/",
+  (req, res, next) => {
+    console.log("1st middleware");
+    // res.send("HAHAHAHA this is a get request");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd middleware");
+    // res.send("2nd middleware");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd middleware");
+    // res.send("3rd middleware");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th middleware");
+    // res.send("4th middleware");
+    next();
+  },
+  (req, res, next) => {
+    console.log("5th middleware");
+    res.send("5th middleware");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+app.listen(3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
