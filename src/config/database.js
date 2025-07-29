26e8bbd39e711ca3d21a224a74f193881322ea74
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-29 10:59:01 +0530

    database auth

--- src/config/database.js
+++ src/config/database.js
@@ -2,17 +2,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const DB_URI = process.env.MONGODB;
+// const DB_URI = process.env.MONGODB;
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://sushant:tKCfEQNzDtKrcBPe@namastenode.mldjppc.mongodb.net/devTinder",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true,
-      tlsInsecure: false,
-    }
-  );
+  await mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    tlsInsecure: false,
+  });
 };
 
 module.exports = { connectDB };
