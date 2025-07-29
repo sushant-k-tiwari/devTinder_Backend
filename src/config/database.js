cdfe995b7f97bb584e0016be4164dad28a299eff
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-29 10:52:05 +0530

    Updated env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const DB_URI = process.env.mongoDB;
 const connectDB = async () => {
